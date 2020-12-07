d8951f7c87d3885ffc667cacf6cf59a4e12b506d
Aryan Gupta <aryanguptaleo@gmail.com>
2020-12-07 07:21:07 +0530

    add build

---
+++ precache-manifest.d0fba542f85fbdfee4565d64a92e6e2a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5004e2980337794212aa9854621d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d8fa876284498e13ca",
+    "url": "/static/css/main.2d2c2cbb.chunk.css"
+  },
+  {
+    "revision": "6a4cfc077cf474b2da7e",
+    "url": "/static/js/2.57321c26.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.57321c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d8fa876284498e13ca",
+    "url": "/static/js/main.506ecf44.chunk.js"
+  },
+  {
+    "revision": "fba0a7ae82e3114643a7",
+    "url": "/static/js/runtime-main.a0576299.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "f9c8b12757db632cbd634271b1cd5b10",
+    "url": "/static/media/data_analytics_.f9c8b127.svg"
+  },
+  {
+    "revision": "8c7ae19756b45657b73055a1142be613",
+    "url": "/static/media/data_process.8c7ae197.svg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "54c3adc538f06c7819b16b934f876911",
+    "url": "/static/media/mountain.54c3adc5.svg"
+  },
+  {
+    "revision": "921df655e6f1ce400074caecfdf30605",
+    "url": "/static/media/process_building.921df655.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  },
+  {
+    "revision": "fafe0d09bc8d6a2b0848c96d52b68f36",
+    "url": "/static/media/sun.fafe0d09.svg"
+  }
+]);
